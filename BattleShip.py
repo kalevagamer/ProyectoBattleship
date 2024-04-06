d8b92f4a4fcec6a915045249be402665c96e33e7
Jose-Arrieta31 <141567964+Jose-Arrieta31@users.noreply.github.com>
2024-04-06 11:23:32 -0600

    BattleShip.py

--- BattleShip.py
+++ BattleShip.py
@@ -9,7 +9,7 @@ class JuegoBatallaNaval:
         self.tablero_jugador2 = [['-' for _ in range(self.columnas)] for _ in range(self.filas)]
         self.tablero_jugador1_objetivo = [['-' for _ in range(self.columnas)] for _ in range(self.filas)]
         self.tablero_jugador2_objetivo = [['-' for _ in range(self.columnas)] for _ in range(self.filas)]
-        self.barcos = {'Destructor': 6, 'Crucero': 4, 'Acorazado': 3}
+        self.barcos = {'Destructor': 6, 'Crucero': 4, 'Acorazado': 2}
         self.barcos_jugador1 = self.colocar_barcos(self.tablero_jugador1)
         self.barcos_jugador2 = self.colocar_barcos(self.tablero_jugador2)
         self.turno = 1
