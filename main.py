021a5e08ceeae193e11ee7a0cf01b3618f47f751
Jose-Arrieta31 <141567964+Jose-Arrieta31@users.noreply.github.com>
2024-04-22 20:52:26 -0600

    Se agrega el master el juego

---
+++ main.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from PIL import Image,ImageTk
+import crearMatriz
+import jugador
+import juego
+import reanudar
+
+matriz=[]
+matrizBotones=[]
+lstJugadores=[]
+
+def bloquearVetana(vtn):
+    vtn.attributes("-disabled", True)
+
+def desBloquearVentana(vtn):
+    vtn.attributes("-disabled", False)
+
+def cargarDatos(vtnMenu):
+    reanudar.reanudarPartida(vtnMenu,matriz,lstJugadores)
+
+def iniciarPartida(vtnMenu):
+    bloquearVetana(vtnMenu)
+    vntjugador=jugador.obternerNombres(vtnMenu,lstJugadores)
+    vtnMenu.wait_window(vntjugador)
+    vtnMatriz=crearMatriz.obtenerTamano(vtnMenu,matriz)
+    vtnMenu.wait_window(vtnMatriz)
+    desBloquearVentana(vtnMenu)
+    vtnMenu.focus_force()
+    juego.crearObjetos(vtnMenu,matriz,lstJugadores,matrizBotones)
+
+
+def main():
+    vtnMenu=Tk()
+    vtnMenu.title("Battleship")
+    vtnMenu.iconbitmap("img/bt.ico")
+    vtnMenu.geometry("1100x650")
+    vtnMenu.resizable(False,False)
+
+    #lables
+    img=Image.open("img/battle.jpeg")
+    img= img.resize((1102,650))
+    fondo=ImageTk.PhotoImage(img)
+    lbl=Label(vtnMenu, image=fondo)
+    lbl.place(x=-5,y=0)
+
+    #botones
+    btnNuevaPartida=Button(vtnMenu,text="Nueva Partida",font=("Arial", 12), command=lambda:iniciarPartida(vtnMenu))
+    btnNuevaPartida.place(x=550-btnNuevaPartida.winfo_reqwidth()//2,y=350-btnNuevaPartida.winfo_reqheight()//2)
+
+    btnReanudarPartida=Button(vtnMenu,text="Reanudar Partida",font=("Arial", 12),command=lambda:cargarDatos(vtnMenu))
+    btnReanudarPartida.place(x=550-btnReanudarPartida.winfo_reqwidth()//2,y=390-btnReanudarPartida.winfo_reqheight()//2)
+
+    btnSalir=Button(vtnMenu,text="Salir",font=("Arial", 12),command=lambda:vtnMenu.destroy())
+    btnSalir.place(x=550-btnSalir.winfo_reqwidth()//2,y=430-btnSalir.winfo_reqheight()//2)
+
+    vtnMenu.mainloop()
+
+
+
+main()
+for e in matriz:
+    print(e)
+for e in lstJugadores:
+    print(e)    
